0d8cb37cd11e426a22ceeb82431ff83455d2eefd
Abush Natanem <abushnatanem@gmail.com>
2025-01-05 18:04:43 +0300

    done with discover page and side bar of the app

--- src/pages/Discover.jsx
+++ src/pages/Discover.jsx
@@ -1,3 +1,61 @@
-const Discover = () => <div>Discover</div>;
+import { useSelector, useDispatch } from "react-redux";
+
+import { Error, Loader } from "../components";
+import { genres } from "../assets/constants";
+
+// NOTE: IMPORTING COMPONENTS
+import SongCard from "../components/SongCard";
+
+// NOTE: IMPORTING APIS
+import { useGetTopChartsQuery } from "../redux/services/shazamCore";
+import { playPause, setActiveSong } from "../redux/features/playerSlice";
+
+const Discover = () => {
+  const dispatch = useDispatch();
+  const { activeSong, isPlaying } = useSelector((state) => state.player);
+
+  const { data, isFetching, error } = useGetTopChartsQuery();
+  console.log(data);
+
+  if (isFetching) return <Loader title="Loading Musics" />;
+  if (error) return <Error />;
+
+  const music = data.item;
+
+  // TODO: functions
+
+  return (
+    <div className="flex flex-col">
+      <div className="w-full flex justify-between items-center sm:flex-row flex-col mt-4 mb-10">
+        <h2 className="font-bold text-3xl text-white text-left">Discover</h2>
+
+        {/* <select
+          onChange={(e) => dispatch(selectGenreListId(e.target.value))}
+          value={genreListId || "pop"}
+          className="bg-black text-gray-300 p-3 text-sm rounded-lg outline-none sm:mt-0 mt-5"
+        >
+          {genres.map((genre) => (
+            <option key={genre.value} value={genre.value}>
+              {genre.title}
+            </option>
+          ))}
+        </select> */}
+      </div>
+
+      <div className="flex flex-wrap sm:justify-start justify-center gap-8">
+        {data?.chart_items.map((song, i) => (
+          <SongCard
+            key={song.item.id}
+            song={song.item}
+            isPlaying={isPlaying}
+            activeSong={activeSong}
+            i={i}
+            data={data.chart_items}
+          />
+        ))}
+      </div>
+    </div>
+  );
+};
 
 export default Discover;
---
+++ src/redux/services/shazamCore.js
@@ -0,0 +1,22 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+
+export const shazamApi = createApi({
+  reducerPath: "shazamApi",
+  baseQuery: fetchBaseQuery({
+    baseUrl: "https://genius-song-lyrics1.p.rapidapi.com",
+    prepareHeaders: (headers) => {
+      headers.set(
+        "x-rapidapi-key",
+        "b931f27379msh036e4c121c518c5p1dbe20jsnacc54fe56000"
+      );
+      headers.set("x-rapidapi-host", "genius-song-lyrics1.p.rapidapi.com"); // API host
+      headers.set("Content-Type", "application/json");
+      return headers;
+    },
+  }),
+  endpoints: (builder) => ({
+    getTopCharts: builder.query({ query: () => "/chart/songs/" }),
+  }),
+});
+
+export const { useGetTopChartsQuery } = shazamApi;
